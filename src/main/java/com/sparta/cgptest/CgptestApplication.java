8562df31a69d30a13b6723a4e1b752825b7677a0
taewan <ntw2100@gmail.com>
2022-06-15 10:26:09 +0900

    Initial commit

---
+++ src/main/java/com/sparta/cgptest/CgptestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.cgptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/cgptest/CgptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.cgptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
